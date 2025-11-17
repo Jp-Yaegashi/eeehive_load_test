159183d3d60ed462c982278c02d9a4fac01412e2
Jp-Yaegashi <yae.dev.jp@gmail.com>
2025-11-17 18:19:59 +0900

    開発

--- Library/influxDB.py
+++ Library/influxDB.py
@@ -3,6 +3,7 @@ from influxdb import InfluxDBClient
 from influxdb.exceptions import InfluxDBClientError, InfluxDBServerError
 from datetime import datetime, timezone
 from Library.debug import logging
+import sys
 
 class influxDB:
     def __init__(self, host, port, database, measurement):
@@ -16,6 +17,8 @@ class influxDB:
         except (InfluxDBClientError, InfluxDBServerError, requests.exceptions.ConnectionError) as e:
             logging.error(f"Error connecting to InfluxDB: {e}")
             self.__client = None
+            print("Error connecting to InfluxDB")
+            sys.exit(1)
 
     def write(self, tags, fields):
         json_body = [
@@ -32,10 +35,16 @@ class influxDB:
             self.__client.write_points(json_body, time_precision='n')
         except (InfluxDBClientError, InfluxDBServerError) as e:
             logging.error(f"Error writing to InfluxDB: {e}")
+            print("Error writing to InfluxDB")
+            sys.exit(1)
         except requests.exceptions.ConnectionError as e:
             logging.error(f"Connection error with InfluxDB: {e}")
+            print("Connection error with InfluxDB")
+            sys.exit(1)
         except Exception as e:
             logging.error(f"Unexpected error writing to InfluxDB: {e}")
+            print("Unexpected error writing to InfluxDB")
+            sys.exit(1)
 
     def delete(self):
         #print("delete")
